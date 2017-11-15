dc8f62d4abf232f34c70f47b369fc3ea71db9e98
Ryan Sperzel <ryansperzel@gmail.com>
2017-11-15 16:29:07 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return {...state, friends: [...state.friends, action.friend]}
+    case 'REMOVE_FRIEND':
+      let newObj = {...state, friends: [...state.friends]}
+      for(var i = 0; i < newObj.friends.length; i++) {
+        if(newObj.friends[i].id === action.id) {
+          newObj.friends.splice(i, 1);
+        }
+      }
+      return newObj
+    default:
+      return state
+  }
 }
