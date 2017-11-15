45183a2d3a7c7a8dd1b4a40072cc1e1238a182c6
Ryan Sperzel <ryansperzel@gmail.com>
2017-11-15 17:05:20 -0500

    Added comment

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -3,6 +3,7 @@ export function manageFriends(state, action){
     case 'ADD_FRIEND':
       return {...state, friends: [...state.friends, action.friend]}
     case 'REMOVE_FRIEND':
+    // WAS doing this: let newObj = {...state, friends: [...state.friends]}, but didn't work since it was destructive - bc when the object had a nested array, it passed by REFERENCE, and destoryed the original state.friends
       let newObj = {...state, friends: [...state.friends]}
       for(var i = 0; i < newObj.friends.length; i++) {
         if(newObj.friends[i].id === action.id) {
